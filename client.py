1827070b40739aeae68cdff068d667678247425f
OriZamir1 <orizamir4@gmail.com>
2026-08-17 23:55:56 +0300

    stage 3 two-way messaging with threads

--- client.py
+++ client.py
@@ -1,9 +1,23 @@
 import socket
+import threading
 
 
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 client_socket.connect(('127.0.0.1', 5000))
+
+def receive_message():
+    while True:
+        data = client_socket.recv(1024)
+        if not data:
+            print("closed connection")
+            break
+        print(f"server: {data.decode()}")
+
+receive_thread = threading.Thread(target=receive_message)
+receive_thread.daemon = True
+receive_thread.start()
+
 
 while True:
     msg = input("you: ")
--- server.py
+++ server.py
@@ -1,4 +1,6 @@
 import socket
+import threading
+
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -8,8 +10,20 @@ server_socket.listen(1)
 
 print("listening")
 
+def send_message():
+    while True:
+        msg = input("server: ")
+        if(msg.lower() == "quit"):
+            break
+        client_socket.send(msg.encode())
+
 client_socket, client_address = server_socket.accept()
 print(f"connected to: {client_address}")
+
+receive_thread = threading.Thread(target=send_message)
+receive_thread.daemon = True
+receive_thread.start()
+
 
 while True:
     data = client_socket.recv(1024)
