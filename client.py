2e0d1f60bc44d438c4f67e20a2e7959de2372b82
OriZamir1 <orizamir4@gmail.com>
2026-08-18 16:06:59 +0300

    stage 4 multi client broadcasting and username id

--- client.py
+++ client.py
@@ -6,13 +6,16 @@ client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 client_socket.connect(('127.0.0.1', 5000))
 
+user_name = input("enter username: ")
+
 def receive_message():
     while True:
         data = client_socket.recv(1024)
         if not data:
             print("closed connection")
             break
-        print(f"server: {data.decode()}")
+        print()
+        print(data.decode())
 
 receive_thread = threading.Thread(target=receive_message)
 receive_thread.daemon = True
@@ -23,6 +26,7 @@ while True:
     msg = input("you: ")
     if msg.lower() == "quit":
         break
-    client_socket.send(msg.encode())
+    full_message = f"{user_name}:  {msg}"
+    client_socket.send(full_message.encode())
 
 client_socket.close()
--- server.py
+++ server.py
@@ -1,35 +1,44 @@
 import socket
 import threading
 
-
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 server_socket.bind(('127.0.0.1', 5000))
 
-server_socket.listen(1)
+server_socket.listen(5)
 
 print("listening")
 
-def send_message():
+clients = []
+
+
+
+
+def handle_client(client_socket, client_address):
     while True:
-        msg = input("server: ")
-        if(msg.lower() == "quit"):
+        data = client_socket.recv(1024)
+        if not data:
             break
-        client_socket.send(msg.encode())
+        for client in clients:
+            if client != client_socket:
+                client.send(data) #we do not encode/decode because we treat the data like a black box we just get data and forward it
+    client_socket.close()
+    clients.remove(client_socket)
+    print(f"disconnected: {client_address}")
 
-client_socket, client_address = server_socket.accept()
-print(f"connected to: {client_address}")
-
-receive_thread = threading.Thread(target=send_message)
-receive_thread.daemon = True
-receive_thread.start()
-
+#stage 3 left over - single client only, not compatible with multiple clients
+# def send_message():
+#     while True:
+#         msg = input("server: ")
+#         if(msg.lower() == "quit"):
+#             break
+#         client_socket.send(msg.encode())
 
 while True:
-    data = client_socket.recv(1024)
-    if not data:
-        break
-    print(f"received {data.decode()}")
+    client_socket, client_address = server_socket.accept()
+    print(f"connected to: {client_address}")
+    clients.append(client_socket)
+    receive_thread = threading.Thread(target=handle_client , args=(client_socket, client_address))
+    receive_thread.daemon = True
+    receive_thread.start()
 
-client_socket.close()
-server_socket.close()
